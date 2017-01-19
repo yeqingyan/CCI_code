71998e9c3ffcf963e13d1bf889f04d94bb8bb12c
YEQING YAN <yyan1@mail.sfsu.edu>
2017-01-18 16:00:44 -0800

    add q3_3

--- Chapter3/q3_3.py
+++ Chapter3/q3_3.py
@@ -16,6 +16,7 @@ class StackOfPlates(object):
         self.THRESHOLD = threshold
         self.current_threshold = 0
         self.top_nodes = None
+        self.num_stacks = 0
 
     def push(self, item):
         new_node = StackOfPlates.StackNode(item)
@@ -23,16 +24,19 @@ class StackOfPlates(object):
         if not self.top_nodes:
             self.top_nodes = StackOfPlates.TopNode(new_node)
             self.current_threshold += 1
-        else: 
+            self.num_stacks += 1
+        else:
             if self.current_threshold >= self.THRESHOLD:    # push to a full stack
                 new_top_node = StackOfPlates.TopNode(new_node)
                 new_top_node.next = self.top_nodes
                 self.top_nodes = new_top_node
-                self.current_threshold = 0
+                self.current_threshold = 1
+                self.num_stacks += 1
             else:   # normal push
                 new_node.next = self.top_nodes.node
                 self.top_nodes.node = new_node
                 self.current_threshold += 1
+        # print("threshold {} stacks {}".format(self.current_threshold, self.num_stacks))
 
     def pop(self):
         if not self.top_nodes:
@@ -42,6 +46,7 @@ class StackOfPlates(object):
             pop_node = self.top_nodes.node
             self.top_nodes = self.top_nodes.next
             self.current_threshold = self.THRESHOLD
+            self.num_stacks -= 1
         else:   # normal pop
             pop_node = self.top_nodes.node
             self.top_nodes.node = self.top_nodes.node.next
@@ -51,8 +56,25 @@ class StackOfPlates(object):
     def peek(self):
         return self.top_nodes.node.val
 
-    def isEmpty():
+    def isEmpty(self):
         return not self.top_nodes
+
+    def popAt(self, index):
+        temp_stack = []
+        size = self.current_threshold + self.THRESHOLD * (self.num_stacks-1)
+        pop_times = size - index
+        if pop_times < 0:
+            return None
+
+        i = 0
+        while i != pop_times: 
+            temp_stack.append(self.pop())
+            i += 1
+        return_node = temp_stack.pop()
+        while temp_stack:
+            self.push(temp_stack.pop())
+        return return_node
+
 
 import random 
 a = StackOfPlates(2)
@@ -60,5 +82,6 @@ for _ in range(10):
     a.push(random.randint(1, 100))
     print(a.peek())
 
-for _ in range(10):
+print("Pop index 4: {}".format(a.popAt(4)))
+for _ in range(9):
     print(a.pop())
